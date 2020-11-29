cc16e8d69ae37770db2f9945d8d5427f312f451c
Hitesh <chavdahitesh2222@gmail.com>
2020-11-29 12:34:51 -0500

    Updated with excel file

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,67 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class LoginPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @FindBy(css = "#navbarDesktop > div > div > ul > li:nth-child(4) > a")
+    private WebElement login;
+    @FindBy(css = "#txt-username")
+    private WebElement userName;
+    @FindBy(css = "#txt-password")
+    private WebElement password;
+    @FindBy(css = "#btn-login > span")
+    private WebElement singin;
+    @FindBy(css = "#newText")
+    private WebElement newMsg;
+    @FindBy(css = "#recipientsView > div > div > input")
+    private WebElement toMsg;
+    @FindBy(css = "#text-input")
+    private WebElement bodyMsg;
+    @FindBy(css = "#send_button")
+    private WebElement sendMsg;
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+        wait = new WebDriverWait(driver,10);
+    }
+    public void loginPageClick() throws InterruptedException {
+        this.login.click();
+//        Thread.sleep(2000);
+    }
+    public void setUserName(){
+        this.userName.sendKeys("chavdahitesh2222@gmail.com");
+    }
+    public void setPassword(){
+        this.password.sendKeys("19051991Hc!");
+    }
+    public void singinclick(){
+        this.singin.click();
+    }
+    public void newMsgClick(){
+        this.newMsg.click();
+    }
+    public LoginPage tomsgClick(String phoneNo){
+//        this.toMsg = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#text-input")));
+        this.toMsg.sendKeys(phoneNo,Keys.ENTER);
+        return this;
+    }
+    public LoginPage toBodyMsgClick(String bodyMsg){
+//        this.bodyMsg = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#text-input")));
+        this.bodyMsg.sendKeys(bodyMsg);
+        return this;
+    }
+    public LoginPage sendBtnClick(){
+        this.sendMsg.click();
+        return this;
+    }
 }
--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -1,4 +1,51 @@
 package testcases;
 
+import data.ExcelDataManager;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
 public class LoginTest {
+
+    @BeforeTest
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Hitesh\\Desktop\\PRAGRA\\chromedriver.exe");
+    }
+    @Test(dataProvider = "contactProvider2",dataProviderClass = ExcelDataManager.class)
+    public void openBrowser(String phoneNo, String bodyMsg) throws InterruptedException{
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://www.textnow.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.loginPageClick();
+        Thread.sleep(2000);
+        loginPage.setUserName();
+        Thread.sleep(2000);
+        loginPage.setPassword();
+        Thread.sleep(2000);
+        loginPage.singinclick();
+        Thread.sleep(5000);
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.ESCAPE);
+        Thread.sleep(2000);
+        loginPage.newMsgClick();
+        Thread.sleep(2000);
+        loginPage.tomsgClick(phoneNo);
+        Thread.sleep(5000);
+        loginPage.toBodyMsgClick(bodyMsg);
+        Thread.sleep(2000);
+        loginPage.sendBtnClick();
+        Thread.sleep(10000);
+        driver.quit();
+    }
+
 }
